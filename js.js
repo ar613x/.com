d52dbfcb051254c7a87147d2dd63519ca76f31be
A. R. <alh.raja613@gmail.com>
2026-07-02 21:13:23 -0400

    wdded mosal

--- js.js
+++ js.js
@@ -12,3 +12,17 @@ for (const el of document.querySelectorAll(".date")) {
 
     el.textContent += ` (${days} day${days !== 1 ? "s" : ""} ago)`;
 }
+
+// modal 
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
